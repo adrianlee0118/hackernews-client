79b3d89601cd334a1bcd0919bea0d5d9d3ca608d
Adrian Lee <adrianlee0118@gmail.com>
2020-07-23 13:43:55 -0700

    split css

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -19,7 +19,7 @@ const SORTS = {
   NONE: (list) => list,
   TITLE: (list) => sortBy(list, "title"),
   AUTHOR: (list) => sortBy(list, "author"),
-  COMMENTS: (list) => sortBy(list, "num_commets").reverse(),
+  COMMENTS: (list) => sortBy(list, "num_comments").reverse(),
   POINTS: (list) => sortBy(list, "points").reverse(),
 };
 
