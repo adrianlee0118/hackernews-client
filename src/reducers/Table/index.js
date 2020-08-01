5de8aae2fc4c19a762218445f8fca73b35b7d8f6
Adrian Lee <adrianlee0118@gmail.com>
2020-07-31 20:03:10 -0700

    added table reducer skeleton

---
+++ src/reducers/Table/index.js
@@ -0,0 +1,10 @@
+import {} from "../../constants";
+
+const tableReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      throw new Error();
+  }
+};
+
+export default tableReducer;
